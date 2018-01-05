bbd3f53e18cf44a792e39f57945d8d6a0287104f
Edward Jones <throne3d@gmail.com>
2018-01-05 01:58:40 +0000

    Fix rspec-page-regression

--- spec/features/css_spec.rb
+++ spec/features/css_spec.rb
@@ -2,10 +2,9 @@ require "spec_helper"
 
 RSpec.feature "Recently updated renders the same", :type => :feature, :js => true do
   before(:each) do
-    puts 'test'
     26.times { create(:post) }
-    puts Post.count
   end
+
   scenario "in default" do
     user = login
     user.update_attributes(layout: 'default')
