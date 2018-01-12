f7e66e40bff8f47fa1e827d751f0c73158494248
Teceler <19716939+teceler@users.noreply.github.com>
2018-01-12 15:08:37 -0600

    Create galleries ordered scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
   attr_writer :validate_password
 
   has_many :icons, -> { order('LOWER(keyword) ASC', created_at: :asc, id: :asc) }
-  has_many :galleries, -> { order('LOWER(name) ASC', created_at: :asc, id: :asc) }
   has_many :characters
+  has_many :galleries
   has_many :character_groups
   has_many :templates
   has_many :sent_messages, class_name: 'Message', foreign_key: 'sender_id'
