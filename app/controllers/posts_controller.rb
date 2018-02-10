e668be2b8788e73abc8db957d95cc98a28c33f72
Teceler <19716939+teceler@users.noreply.github.com>
2018-02-10 12:47:57 -0600

    Check writable properties / reply is saved

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -122,7 +122,7 @@ class PostsController < WritableController
     reply = @post.replies.new(written_params)
     reply.user = current_user
 
-    unless @post.save
+    unless @post.save && reply.save
       flash.now[:error] = {}
       flash.now[:error][:array] = @post.errors.full_messages
       flash.now[:error][:message] = "Your post could not be saved because of the following problems:"
